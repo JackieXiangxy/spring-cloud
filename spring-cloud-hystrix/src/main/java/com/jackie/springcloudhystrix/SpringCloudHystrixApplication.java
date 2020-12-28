1d4c92a89d42a854d61ebffaaa79f0765ede1c5f
jackie <jackiexiangxy@gmail.com>
2020-12-28 18:26:55 +0800

    添加hystris模块

---
+++ spring-cloud-hystrix/src/main/java/com/jackie/springcloudhystrix/SpringCloudHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.jackie.springcloudhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-hystrix/src/test/java/com/jackie/springcloudhystrix/SpringCloudHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackie.springcloudhystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHystrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
